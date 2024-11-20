3603931e28ed949726f5f12b9538486c90160f31
nandab1805 <balabhadranandakiranroyal@gmail.com>
2024-11-20 18:29:37 +0530

    Adding terraform scripts

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 module "roboshop" {
-    source = "../terraform-aws-vpc" 
+   #  source = "../terraform-aws-vpc"
+    source = "git::https://github.com/nandab1805/terraform-aws-vpc.git?ref=main" 
     project_name = var.project_name
     environment = var.environment
     common_tags = var.common_tags
